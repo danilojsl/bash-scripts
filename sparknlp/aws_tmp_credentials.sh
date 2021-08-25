b39dbd248b7915dcf5f2fb4d7d50519b5b67f1ac
Danilo Burbano <danilo@johnsnowlabs.com>
2021-08-25 11:32:06 -0500

    Adding bash script to get temporal AWS credentials with MFA

---
+++ sparknlp/aws_tmp_credentials.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "MFA device argument not supplied"
+    exit 1
+  else
+   # The value is either the serial number for a hardware device (such as GAHT12345678 ) or an Amazon Resource Name (ARN) for a virtual device (such as arn:aws:iam::123456789012:mfa/user).
+   MFA_DEVICE_ID=$1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "Duration argument not supplied"
+  else
+    DURATION=$2 # The duration, in seconds, that the credentials should remain valid
+fi
+
+if [ "$DURATION" -lt 900 ]
+  then
+    echo "Acceptable durations for IAM user sessions range from 900 seconds. Setting to this value..."
+    DURATION=900
+fi
+
+if [ -z "$3" ]
+  then
+    echo "Token argument not supplied"
+    exit 1
+  else
+   TOKEN=$3  # The value provided by the MFA device
+fi
+
+echo "Getting temporal AWS credentials for $MFA_DEVICE_ID"
+
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+
+aws sts get-session-token --serial-number $MFA_DEVICE_ID --duration-seconds $DURATION --token-code $TOKEN |
+    jq -r \
+        --arg aki "aws configure set aws_access_key_id" \
+        --arg asak "aws configure set aws_secret_access_key" \
+        --arg ast "aws configure set aws_session_token" \
+    '.Credentials|($aki + " " + .AccessKeyId),($asak + " " + .SecretAccessKey),($ast + " " + .SessionToken)'
+
+# TODO: Create variables to reference on export
+echo "AWS_ACCESS_KEY_ID=$aki"
+export AWS_ACCESS_KEY_ID=$aki
+export AWS_SECRET_ACCESS_KEY=$asak
+export AWS_SESSION_TOKEN=$ast
+
+DURATION_IN_MINUTES=$(awk "BEGIN {print $DURATION / 60}")
+echo "Credentials remain valid for $DURATION_IN_MINUTES minutes"
